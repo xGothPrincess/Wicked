68df7626d2be8929803d83842994a7e70acec335
xGothPrincess <83084669+xGothPrincess@users.noreply.github.com>
2026-08-21 12:31:48 +0200

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -93,8 +93,8 @@ export const botConfig = {
   embeds: {
     colors: {
       // Main brand colors.
-      primary: "#FFFFFF",
-      secondary: "#2F3136",
+      primary: "#000000",
+      secondary: "#880808",
 
       // Standard status colors for success/error/warning/info messages.
       success: "#57F287",
